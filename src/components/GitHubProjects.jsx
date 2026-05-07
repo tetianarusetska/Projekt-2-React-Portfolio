d4f25ba22b92ee19bf5888e43afbf2fac28459c6
Rusetska Tetiana <rusetskatata@gmail.com>
2026-05-07 11:54:44 +0200

    add github projects

--- src/components/GitHubProjects.jsx
+++ src/components/GitHubProjects.jsx
@@ -14,10 +14,41 @@ export default function GitHubProjects({ username }) {
     }, []);
 
     return (
-        <ul>
-            {repositories.slice(0, 3).map((repo) => (
-                <li key={repo.id}>{repo.name} {repo.description}</li>
-            ))}
-        </ul>
+        <div className="hidden md:block md:flex md:flex-col md:justify-center md:items-center md:border-1 md:py-[40px]
+        md:mt-[100px]
+        md:mt-[100px]
+        md:gap-[40px]
+        ">
+            <h1 className="md:font-[Boxing]
+            md:text-[42px]
+            ">Projekte & Deployments</h1>
+            <ul className="md:font-[Montserrat] md:flex md:flex-col 
+            md:gap-[18px]
+            md:text-[20px]
+            ">
+                {repositories.slice(0, 3).map((repo) => (
+                    <li className="md:flex md:flex-row md:gap-[10px] md:gap-[30px]" key={repo.id}>
+                        {repo.name}
+                        <a
+                            href={repo.html_url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Auf GitHub ansehen
+                        </a>
+                        {repo.homepage && (
+                            <a href={repo.homepage} target="_blank" rel="noopener noreferrer">
+                                Live auf Vercel ansehen
+                            </a>
+                        )}
+                    </li>
+                ))}
+            </ul>
+            <a className="md:font-[Montserrat]
+            mt-[20px] md:mt-[0px]
+            md:text-[20px]
+            "
+                href="https://github.com/tetianarusetska">Mehr Projekte auf GitHub entdecken</a>
+        </div>
     );
 }
